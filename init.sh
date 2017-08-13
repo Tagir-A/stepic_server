735dbe4b548bc2b548776e522df0bb8749365c35
Almasov Tagir <tagir-a@Almasovs-MacBook-Pro.local>
2017-08-13 10:11:13 +0300

    wsgi

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
+sudo apt-get update
+sudo apt-get install nginx
+sudo apt-get install gunicorn
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
